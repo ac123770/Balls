943d1003cc02e92fc09c8fde55a2de4aae09c75b
karim ashroff <karimashroff@gmail.com>
2020-08-31 16:19:10 -0400

    added Evil Circle

--- main.js
+++ main.js
@@ -25,6 +25,54 @@ this.color = color;
 this.size = size;
 }
 
+function EvilCircle(x, y, exists) {
+  Shape.call(this, x, y, 20, 20, exists);
+  this.color = "white";
+  this.size = 10;
+}
+
+EvilCircle.prototype.draw = function(){
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+EvilCircle.prototype.checkBounds = function(){
+
+}
+
+EvilCircle.prototype.setControls = function(){
+  let _this = this;
+  window.onkeydown = function(e) {
+      if (e.key === 'a') {
+        _this.x -= _this.velX;
+      } else if (e.key === 'd') {
+        _this.x += _this.velX;
+      } else if (e.key === 'w') {
+        _this.y -= _this.velY;
+      } else if (e.key === 's') {
+        _this.y += _this.velY;
+      }
+    }
+}
+EvilCircle.prototype.collisionDetect = function(){
+  for (let j = 0; j < balls.length; j++) {
+    if ((balls[j].exists)) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists = false;
+      }
+    }
+  }
+
+}
+
+
 Ball.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
@@ -69,14 +117,24 @@ function loop() {
   ctx.fillRect(0, 0, width, height);
 
   for (let i = 0; i < balls.length; i++) {
+    if (balls[i].exists){
     balls[i].draw();
     balls[i].update();
     balls[i].collisionDetect();
   }
 
+evilCircle.draw()
+evilCircle.checkBounds()
+evilCircle.collisionDetect()
+
+
+  }
   requestAnimationFrame(loop);
 }
 let balls = [];
+evilCircle = new EvilCircle(100, 100, true);
+
+evilCircle.setControls();
 
 while (balls.length < 25) {
   let size = random(10,20);
